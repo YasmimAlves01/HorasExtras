75d275543400d20fda5f58dd86004a669fc9da7d
YasmimAlves01 <yasmimalvesrodrigues16@gmail.com>
2025-05-17 20:48:20 -0300

    Calculo das horas extras

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿double valorHora, horasTrabalhadas, horaExtra;
+
+
+Console.WriteLine("Bem-vindo ao Calculo de Salario");
+
+Console.WriteLine("Por favor informe o valor que você recebe por hora:");
+valorHora = Convert.ToDouble(Console.ReadLine()!);
+
+Console.WriteLine("Por favor agora informe quantas horas você trabalhou:");
+horasTrabalhadas = Convert.ToDouble(Console.ReadLine()!);
+
+Console.WriteLine("Por favor informe a quantidade de horas extras que você tem:");
+horaExtra = Convert.ToDouble(Console.ReadLine()!);
+
+
+double CalculaSalario(double valorHora, double horasTrabalhadas, double horaExtra)
+{
+    double totalSalario = horasTrabalhadas * valorHora;
+
+    double totalHoraExtra = valorHora * (horaExtra * 1.4);
+
+    return totalHoraExtra + totalSalario;
+
+}
+
+
+double salarioFinal = CalculaSalario(valorHora, horasTrabalhadas, horaExtra);
+Console.WriteLine($"Seu salario total é:{salarioFinal}");
